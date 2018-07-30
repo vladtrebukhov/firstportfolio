ef13d480da787f1251c9172b76d162c1d6da2b15
Vlad Trebukhov <vladtrebukhov123@gmail.com>
2018-07-30 00:29:33 -0400

    added form functionality

--- script.js
+++ script.js
@@ -7,5 +7,25 @@ $(document).ready(function () {
   });
 })
 
+// send form information using an AJAX request to formspree (which redirects to personal email)
+var form = document.getElementById('main-form');
 
+form.addEventListener('submit', function (event) {
+  let name = form.name.value;
+  let email = form.email.value;
+  let message = form.message.value;
 
+  if (!name || !email || !message) {
+    alert('Please check your entries')
+    event.preventDefault();
+  } else {
+    $.ajax({
+      url: 'https://formspree.io/vladtrebukhov123@gmail.com',
+      method: 'POST',
+      data: $(this).serialize(),
+      dataType: 'json'
+    })
+    alert('Thank you, your message has been sent.');
+    form.reset();
+  }
+})
