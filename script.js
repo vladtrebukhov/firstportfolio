8763936b61d6881ccb8e6b7cdfa5a40555454eef
Vlad Trebukhov <vladtrebukhov123@gmail.com>
2018-09-10 22:26:30 -0400

    stable version

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 // play/pause video on mouse hover
 $(document).ready(function () {
-  $('.video').on('mouseover', function (event) {
-    this.play();
-  }).on('mouseout', function (event) {
-    this.pause();
-  });
-})
+  $('.video')
+    .on('mouseover', function (event) {
+      this.play();
+    })
+    .on('mouseout', function (event) {
+      this.pause();
+    });
+});
 
 // send form information using an AJAX request to formspree (which redirects to personal email)
 var form = document.getElementById('main-form');
@@ -16,7 +18,7 @@ form.addEventListener('submit', function (event) {
   let message = form.message.value;
 
   if (!name || !email || !message) {
-    alert('Please check your entries')
+    alert('Please check your entries');
     event.preventDefault();
   } else {
     $.ajax({
@@ -24,8 +26,8 @@ form.addEventListener('submit', function (event) {
       method: 'POST',
       data: $(this).serialize(),
       dataType: 'json'
-    })
+    });
     alert('Thank you, your message has been sent.');
     form.reset();
   }
-})
+});
